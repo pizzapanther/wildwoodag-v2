db52d5619329c4202e7bef06050c0ecc66d615ab
Paul Bailey <paul@neutron.studio>
2023-08-31 15:27:17 -0500

    native app setup

--- prepare_native_app.py
+++ prepare_native_app.py
@@ -1,17 +1,28 @@
 #!/usr/bin/env python3
 
 import os
-
+import re
 
 def run():
   base = "public"
-  index_paths = [
-    '/about/',
-    '/events/',
-    '/watch/',
-    '/donate/',
-    '/contact/'
+  html = [
+    [r'href="/(.*)/"', r'href="/\1/index.html"'],
+    [r"href='/(.*)/'", r"href='/\1/index.html'"],
   ]
+
+  json = [
+    [r'"permalink":\s*"/(.*?)/"', r'"permalink":"/\1/index.html"'],
+  ]
+
+  path = "public/search/index.json"
+  with open(path, 'r') as fh:
+    text = fh.read()
+
+  for s in json:
+    text = re.sub(s[0], s[1], text, flags=re.I)
+
+  with open(path, 'w') as fh:
+    fh.write(text)
 
   for root, dirs, files in os.walk(base):
     for f in files:
@@ -21,8 +32,8 @@ def run():
         with open(path, 'r') as fh:
           text = fh.read()
 
-        for i in index_paths:
-          text = text.replace(i, f"{i}index.html")
+        for s in html:
+          text = re.sub(s[0], s[1], text, flags=re.I)
 
         with open(path, 'w') as fh:
           fh.write(text)
