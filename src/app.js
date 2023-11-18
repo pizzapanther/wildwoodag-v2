bcac825933c3b2e948ba486061c7c23b35c4ec49
Paul Bailey <paul@neutron.studio>
2023-11-18 14:26:00 -0600

    update modal

--- src/app.js
+++ src/app.js
@@ -1,4 +1,22 @@
 import { CapacitorUpdater } from '@capgo/capacitor-updater';
 
+import { Dialog } from '@capacitor/dialog'
+
+CapacitorUpdater.addListener('updateAvailable', async (res) => {
+  try {
+    const { value } = await Dialog.confirm({
+      title: 'Update Available',
+      message: `Version ${res.bundle.version} is available. Would you like to update now?`,
+    })
+
+    if (value)
+      CapacitorUpdater.set(res.bundle)
+
+  }
+  catch (error) {
+    console.log(error)
+  }
+})
+
 CapacitorUpdater.notifyAppReady();
 console.log('CapacitorUpdater Loaded');
