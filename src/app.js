aa096cfb1bb14aead0d533729bf48969e6e9ca11
Paul Bailey <paul@neutron.studio>
2024-09-26 06:26:07 -0500

    updated build

--- src/app.js
+++ src/app.js
@@ -1,22 +1,23 @@
-import { CapacitorUpdater } from '@capgo/capacitor-updater';
+import { CapacitorUpdater } from "@capgo/capacitor-updater";
 
-import { Dialog } from '@capacitor/dialog'
-
-CapacitorUpdater.addListener('updateAvailable', async (res) => {
+import { Dialog } from "@capacitor/dialog";
+console.log("NARF");
+CapacitorUpdater.addListener("updateAvailable", async (res) => {
+  console.log("PRE RES", res);
   try {
     const { value } = await Dialog.confirm({
-      title: 'Update Available',
+      title: "Update Available",
       message: `Version ${res.bundle.version} is available. Would you like to update now?`,
-    })
+    });
 
-    if (value)
-      CapacitorUpdater.set(res.bundle)
-
+    console.log("RES", res);
+    if (value) {
+      CapacitorUpdater.set(res.bundle);
+    }
+  } catch (error) {
+    console.log("UPDATER ERROR", error);
   }
-  catch (error) {
-    console.log(error)
-  }
-})
+});
 
 CapacitorUpdater.notifyAppReady();
-console.log('CapacitorUpdater Loaded');
+console.log("CapacitorUpdater Loaded");
